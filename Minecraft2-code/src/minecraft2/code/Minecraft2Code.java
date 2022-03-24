7e5e716ba18dca067ee370d869da0a1705b29542
Saruken <daniel.gomez.garcia3300@gmail.com>
2022-03-24 13:56:19 +0100

    Segundo commit

--- Minecraft2-code/src/minecraft2/code/Minecraft2Code.java
+++ Minecraft2-code/src/minecraft2/code/Minecraft2Code.java
@@ -4,6 +4,7 @@ public class Minecraft2Code {
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("Adios mundo cruel");
     }
     
 }
