0b1761f488e58b72f5f4afce7c4d0678e870e57b
Saruken <daniel.gomez.garcia3300@gmail.com>
2022-03-24 13:58:24 +0100

    Mayonesa

--- Minecraft2-code/src/minecraft2/code/Minecraft2Code.java
+++ Minecraft2-code/src/minecraft2/code/Minecraft2Code.java
@@ -5,6 +5,7 @@ public class Minecraft2Code {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Adios mundo cruel");
+        System.out.println("Mayonesa");
     }
     
 }
